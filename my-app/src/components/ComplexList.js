ffb24928737b1da9ea44edcb5d7d482b6cb2d4a5
Kristina <kristinaodavidson@gmail.com>
2019-05-20 20:05:34 -0600

    my-app updates

---
+++ my-app/src/components/ComplexList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const list = [
+  {
+    id: 'a',
+    firstname: 'Robin',
+    lastname: 'Wieruch',
+    year: 1988,
+  },
+  {
+    id: 'b',
+    firstname: 'Dave',
+    lastname: 'Davidds',
+    year: 1990,
+  },
+];
+
+const ComplexList = () => (
+  <ul>
+    {list.map(item => (
+      <li key={item.id}>
+        <div>{item.id}</div>
+        <div>{item.firstname}</div>
+        <div>{item.lastname}</div>
+        <div>{item.year}</div>
+      </li>
+    ))}
+  </ul>
+);
+
+export default ComplexList;
